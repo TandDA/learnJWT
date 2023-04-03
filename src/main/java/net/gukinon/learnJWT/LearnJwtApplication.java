1378f3da0f463136903f97f1da206ad1b34e46fe
TandDA <tanduevdmitriy@gmail.com>
2023-04-03 21:55:20 +0300

    Initial commit

---
+++ src/main/java/net/gukinon/learnJWT/LearnJwtApplication.java
@@ -0,0 +1,13 @@
+package net.gukinon.learnJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/gukinon/learnJWT/LearnJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gukinon.learnJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
